017131ff84af2d9db1442aacc68c13dcd5c8d6c4
Sanskar Wakchaure <144128761+MynameisSanskar@users.noreply.github.com>
2024-08-20 20:47:16 +0530

    Time: 0 ms (100%), Space: 7.6 MB (56.27%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    int f(int num,int cpy,int n){
-        if(num==n)
-        return 0;
-        if(num+cpy>n)
-        return 2000;
-        int c=2+f(num+num,num,n);
-        int p=1+f(num+cpy,cpy,n);
-        return min(c,p);
-    }
     int minSteps(int n) {
         if(n<=1)
         return 0;
-     return 2+f(2,1,n);   
+        int steps=0;
+        int factor=2;
+        while(n>1){
+            while(n%factor==0){
+                steps+=factor;
+                n/=factor;
+            }
+            factor++;
+        }
+        return steps;
     }
 };

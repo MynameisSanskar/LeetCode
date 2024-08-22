47a7ac00dc81e9c283f8210124b6cdab4123999a
Sanskar Wakchaure <144128761+MynameisSanskar@users.noreply.github.com>
2024-08-22 14:43:37 +0530

    Time: 0 ms (100%), Space: 7.6 MB (33.08%) - LeetHub

---
+++ 0476-number-complement/0476-number-complement.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int findComplement(int num) {
+       string binary="";
+       while(num!=0){
+        binary.push_back((num%2)+'0');
+        num/=2;
+       }
+       cout<<binary;
+       reverse(binary.begin(),binary.end());
+       for(int i=0;i<binary.size();i++){
+        binary[i]=(binary[i]=='1')?'0':'1';
+       }
+       int n=0; 
+
+       for(int i=0;i<binary.size();i++){
+        n=n*2+(binary[i]-'0');
+       }
+       return n;
+    }
+};

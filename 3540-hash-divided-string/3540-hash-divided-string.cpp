67a5bf1dca9de10c29f913dc94369fc2ab93ae8d
Sanskar Wakchaure <144128761+MynameisSanskar@users.noreply.github.com>
2024-09-03 21:40:26 +0530

    Time: 15 ms (15.67%), Space: 10.4 MB (63.05%) - LeetHub

--- 3540-hash-divided-string/3540-hash-divided-string.cpp
+++ 3540-hash-divided-string/3540-hash-divided-string.cpp
@@ -4,7 +4,9 @@ public:
     string res  = "";
     for(int i = 0, sum = 0; i < s.size(); ++i){
         sum += s[i] - 'a';
-        if((i+1)%k == 0) { res += ('a' + (sum %26));  sum = 0; }
+        if((i+1)%k == 0) {
+             res += ('a' + (sum %26));
+               sum = 0; }
     }
     return res;
 }
